3ae57df3e4403c24251c7718b09285a00f48d5d3
botyto <biserkrustev.botyto@gmail.com>
2023-09-15 17:23:48 +0300

    [Backend][Data] SQL prints for debug builds

--- backend/core/data/sql/database.py
+++ backend/core/data/sql/database.py
@@ -1,9 +1,15 @@
+from datetime import datetime, timezone
+import logging
 from sqlalchemy import create_engine, Engine
+import sqlalchemy.event
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
+from types import MethodType
 
 from .settings import SqlSettings
 
 from ..context import DataContext
+
+logger = logging.getLogger(__name__)
 
 
 class Model(DeclarativeBase):
@@ -23,6 +29,8 @@ class Database:
             Model.metadata.create_all(self.engine)
         if wipe_settings:
             self.context.sql.wipe()
+        if self.context.env.debug:
+            self._activate_debug()
 
     @property
     def connection_string(self):
@@ -39,3 +47,21 @@ class Database:
 
     def make_session(self, info: dict|None = None) -> Session:
         return self._sessionmaker(info=info)
+    
+    def _activate_debug(self):
+        def on_connect(dbapi_con, connection_record):
+            logger.debug("Connecting to database: %s", dbapi_con)
+        sqlalchemy.event.listen(self.engine, "connect", on_connect)
+        
+        def on_detch(dbapi_con, connection_record):
+            logger.debug("Disconnecting from database: %s", dbapi_con)
+        sqlalchemy.event.listen(self.engine, "detach", on_detch)
+
+        def on_before_execute(conn, clauseelement, multiparams, params, execution_options):
+            sql = str(clauseelement).replace("\n", " ")
+            logger.debug("Executing SQL: %s", sql)
+        sqlalchemy.event.listen(self.engine, "before_execute", on_before_execute)
+
+        for cls in Model.__subclasses__():
+            table_name = getattr(cls, "__tablename__", getattr(cls, "__table__"))
+            assert table_name == cls.__name__, "Table name must be the same as the model class name"
