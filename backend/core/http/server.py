da16e4d25924dfef91af0044227101866bf01f74
botyto <biserkrustev.botyto@gmail.com>
2023-09-15 13:44:48 +0300

    [Backend][Server] Setup server

--- backend/core/http/server.py
+++ backend/core/http/server.py
@@ -1,22 +1,55 @@
 import logging
 import tornado.ioloop
-import tornado.web
+from tornado.platform.asyncio import AsyncIOMainLoop
+from tornado.routing import URLSpec
+from tornado.web import Application
+from typing import cast
 
 from .context import ServerContext
+
+from .. import msg
 
 logger = logging.getLogger(__name__)
 
 
 class Server:
     context: ServerContext
-    app: tornado.web.Application
+    loop: AsyncIOMainLoop
+    app: Application
 
     def __init__(self, context: ServerContext):
         self.context = context
+        self.app = Application(
+            handlers=self._gather_handlers(),
+            template_path="templates",
+            static_path="static",
+            cookie_secret=self.env.get("COOKIE_SECRET", "PLACEHOLDER"),
+            compiled_template_cache=True,
+            static_hash_cache=True,
+            compress_response=True,
+            debug=self.env.debug,
+            autoreload=False,
+            server_context=self.context,
+        )
+        self.loop = cast(AsyncIOMainLoop, tornado.ioloop.IOLoop.current())
+        if self.env.debug:
+            self.loop.asyncio_loop.set_debug(True)
+
+    def _gather_handlers(self):
+        handlers = []
+        msg.emit("gather_http_handlers", handlers)
+        assert(all(isinstance(h, URLSpec) for h in handlers))
+        return handlers
 
     def run(self):
-        port = self.context.port
-        self.app = tornado.web.Application()
-        logger.info("Listening on port %d", port)
-        self.app.listen(port)
-        tornado.ioloop.IOLoop.current().start()
+        logger.info("Listening on port %d", self.port)
+        self.app.listen(self.port)
+        self.loop.start()
+
+    @property
+    def port(self):
+        return self.context.port
+
+    @property
+    def env(self):
+        return self.context.env
