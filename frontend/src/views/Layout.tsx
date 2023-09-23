541ee93e382b5e3e00fe76af3ba6ef2600ff415a
biser <bisserkr@gmail.com>
2023-09-23 20:21:21 +0300

    [Frontend] Nav button

--- frontend/src/views/Layout.tsx
+++ frontend/src/views/Layout.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavButton from "../components/NavButton";
 
 export default function Layout() {
-	return <Outlet/>;
+	return <>
+		<Outlet/>
+		<div style={{position: "fixed", bottom: "25px", right: "25px"}}>
+			<NavButton/>
+		</div>
+	</>;
 }
