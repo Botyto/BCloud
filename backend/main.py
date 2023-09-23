d748c97edb52689c6257d2122886a87148a9ece8
biser <bisserkr@gmail.com>
2023-09-23 18:47:44 +0300

    [Backend][Sql] Initial migration

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,17 @@ from core.main import MigrationAction, run_migration, run_app
 parser = ArgumentParser(
     prog="Backend server",
     description="Backend server")
-parser.add_argument("-m", "--migration",
+parser.add_argument("-m",
     dest="migration",
     required=False,
     type=str,
     choices=[action.value for action in MigrationAction])
+parser.add_argument("-t", dest="title", required=False, type=str)
 args = parser.parse_args()
 
 if __name__ == '__main__':
     if args.migration:
         action = MigrationAction(args.migration)
-        run_migration(action)
+        run_migration(action, args.title)
     else:
         run_app()
