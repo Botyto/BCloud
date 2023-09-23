21441a4d7a26c87dfd883d379a4d32ff83455579
biser <bisserkr@gmail.com>
2023-09-23 20:06:22 +0300

    [Frontend] Use the loading component

--- frontend/src/views/Homepage.tsx
+++ frontend/src/views/Homepage.tsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { gql, useQuery } from '@apollo/client';
+import Loading from '../components/Loading';
 
 const USER = gql`
 query CurrentUser {
     profileAuthUser {
         displayName
     }
-}`
+}`;
 
 export default function Homepage() {
     const currentUserVars = useQuery(USER);
     if (currentUserVars.error) {
         return <span>Hi <span style={{color: "red"}}>{currentUserVars.error.message}</span></span>;
+    } else if (currentUserVars.loading) {
+        return <span>Hi <Loading/></span>
     } else if (currentUserVars.data) {
         return <span>Hi {currentUserVars.data.profileAuthUser?.displayName || "MISSING USER"}</span>;
     } else {
