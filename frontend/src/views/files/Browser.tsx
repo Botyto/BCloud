d3192d3e0ee361d1b351f7ac2917afa118faf402
botyto <biserkrustev.botyto@gmail.com>
2023-10-02 15:39:35 +0300

    [Frontend] Breadcrumbs fix

--- frontend/src/views/files/Browser.tsx
+++ frontend/src/views/files/Browser.tsx
@@ -7,9 +7,16 @@ export default function Browser() {
     const storageId = params.storageId || "";
     const filePath = params["*"] || "";
     const [_, parts] = fspath.getParts(filePath);
-    parts[0] = fspath.sep + parts[0];
+    if (parts.length > 0) {
+        parts[0] = fspath.sep + parts[0];
+    }
     return <>
         <div>Browser (<Link to="/files">storages</Link>)</div>
         <div>Path: {fspath.join(storageId, parts)}</div>
+        <div>
+            <ol>
+                ... files here ...
+            </ol>
+        </div>
     </>;
 }
