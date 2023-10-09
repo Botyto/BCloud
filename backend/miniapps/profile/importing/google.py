ed4b3bd70a40500ccaea0463273609f855bebe9f
botyto <biserkrustev.botyto@gmail.com>
2023-10-09 17:29:31 +0300

    [Backend][Profile] Progress on google importing

--- backend/miniapps/profile/importing/google.py
+++ backend/miniapps/profile/importing/google.py
@@ -1,11 +1,109 @@
+import asyncio
 from dataclasses import dataclass
-from typing import Callable, Set
+import itertools
+import os.path
+from typing import cast, List, Set, Coroutine
 
-from googleapiclient.discovery import Resource
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build, Resource
+from google.oauth2.credentials import Credentials
+from google.auth.external_account_authorized_user import Credentials as ExternalCredentials
+
+from core.api.modules.gql import mutation
+from core.api.modules.rest import get, ApiContext
+from core.asyncjob.handlers import Action, State
+
+
+class GoogleImporter:
+    SERVICE: str
+    SCOPES: Set[str]
+
+    async def run(self, service: Resource, payload: dict):
+        raise NotImplementedError()
 
 
 @dataclass
-class GoogleImporter:
-    service: str
-    scopes: Set[str]
-    callback: Callable[[Resource], None]
+class GoogleAuthUrl:
+    url: str
+
+
+class BaseGoogleImporting:
+    context: ApiContext
+
+    @property
+    def _google_client_secrets(self):
+        return cast(str, self.context.env.get("GOOGLE_CLIENT_SECRETS", ""))
+
+    @classmethod
+    def _google_all_importers(cls):
+        return GoogleImporter.__subclasses__()
+    
+    @classmethod
+    def _google_all_scopes(cls):
+        all_scopes = set()
+        for importer in cls._google_all_importers():
+            all_scopes.update(importer.SCOPES)
+        return all_scopes
+    
+    @classmethod
+    def _google_make_importers(cls):
+        return [i() for i in cls._google_all_importers()]
+
+    @classmethod
+    def _google_make_service(cls, service: str, creds: Credentials|ExternalCredentials) -> Resource:
+        return build(service, "v3", credentials=creds)
+
+    @classmethod
+    def _google_make_flow(cls, client_secrets: str):
+        if not os.path.isfile(client_secrets):
+            raise Exception("Google services aren't setup")
+        return InstalledAppFlow.from_client_secrets_file(client_secrets, cls._google_all_scopes())
+
+
+class GqlGoogleImporting(BaseGoogleImporting):
+    @mutation()
+    def google_init(self) -> GoogleAuthUrl:
+        flow = self._google_make_flow(self._google_client_secrets)
+        flow.redirect_uri = "localhost:3000/profile/importing/google"
+        url, state = flow.authorization_url(access_type="offline", include_granted_scopes="true")
+        return GoogleAuthUrl(url)
+
+
+class RestGoogleImporting(BaseGoogleImporting):
+    @get("/profile/importing/google/(.*)")
+    def start_google_importing(self, auth_code: str):
+        flow = self._google_make_flow(self._google_client_secrets)
+        flow.fetch_token(code=auth_code)
+        creds = flow.credentials
+        self.context.asyncjobs.schedule("profile", "importing.google", {
+            "user_id": self.context.user_id,
+            "creds": creds,
+        })
+
+    @staticmethod
+    async def __wrapper(state: State, dprogress: float, importer: GoogleImporter, service: Resource, payload: dict):
+        await importer.run(service, payload)
+        state.set_progress(state.progress + dprogress)
+
+    @classmethod
+    def _google_import(cls, action: Action, state: State|None, id: int, payload: dict):
+        if action != Action.RUN:
+            return
+        assert state is not None
+        creds = payload["creds"]
+        all_importers = cls._google_make_importers()
+        by_service = itertools.groupby(all_importers, lambda i: i.SERVICE)
+        tasks: List[Coroutine] = []
+        dprogress = 1.0 / len(all_importers)
+        for service_name, importers in by_service:
+            service = cls._google_make_service(service_name, creds)
+            for importer in importers:
+                wrapper = RestGoogleImporting.__wrapper(state, dprogress, importer, service, payload)
+                tasks.append(wrapper)
+        import_task = asyncio.gather(*tasks, return_exceptions=True)
+        try:
+            asyncio.get_event_loop().run_until_complete(import_task)
+        except Exception as e:
+            state.set_error(str(e))
+        else:
+            state.complete()
--- backend/miniapps/profile/importing/module.py
+++ backend/miniapps/profile/importing/module.py
@@ -20,26 +20,43 @@ class GoogleImporter:
     callback: Callable[[Resource], None]
 
 
+@dataclass
+class GoogleAuthUrl:
+    url: str
+
 
 class ImportingModule(GqlMiniappModule):
     def _make_google_service(self, service: str, creds: Credentials|ExternalCredentials) -> Resource:
         return build(service, "v3", credentials=creds)
 
-    @mutation()
-    def google(self) -> SuccessResult:
+    def _make_google_flow(self):
         google_client_secerts = cast(str, self.context.env.get("GOOGLE_CLIENT_SECRETS", ""))
         if not os.path.isfile(google_client_secerts):
             raise Exception("Google services aren't setup")
         all_importers: List[GoogleImporter] = []
         self.context.msg.emit("importing_gather_google", all_importers)
         if not all_importers:
-            return SuccessResult()
+            raise Exception("Nothing to import from Google")
         assert all(isinstance(i, GoogleImporter) for i in all_importers), "Importers must be GoogleImporter"
         all_scopes = set()
         for importer in all_importers:
             all_scopes.update(importer.scopes)
-        flow = InstalledAppFlow.from_client_secrets_file(google_client_secerts, all_scopes)
-        creds = flow.run_local_server(port=0)
+        return InstalledAppFlow.from_client_secrets_file(google_client_secerts, all_scopes)
+
+    @mutation()
+    def google_init(self) -> GoogleAuthUrl:
+        flow = self._make_google_flow()
+        flow.redirect_uri = "localhost:3000/profile/importing/google"
+        url, state = flow.authorization_url(access_type="offline", include_granted_scopes="true")
+        return GoogleAuthUrl(url)
+
+    @mutation()
+    def google_auth(self, auth_code: str) -> SuccessResult:
+        flow = self._make_google_flow()
+        flow.fetch_token(code=auth_code)
+        creds = flow.credentials
+        all_importers: List[GoogleImporter] = []
+        self.context.msg.emit("importing_gather_google", all_importers)
         by_service = itertools.groupby(all_importers, lambda i: i.service)
         for service_name, importers in by_service:
             service = self._make_google_service(service_name, creds)
