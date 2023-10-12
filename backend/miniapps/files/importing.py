6bf92a2b7a1f94016e572f80b5fd0747a61a71e1
botyto <biserkrustev.botyto@gmail.com>
2023-10-12 16:43:55 +0300

    [Backend][Files] Fix importing google-specific files

--- backend/miniapps/files/importing.py
+++ backend/miniapps/files/importing.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import io
 import logging
 import pickle
-from typing import List
+from typing import Callable, List
 
 from .tools import fspath
 from .tools.errors import FileAlreadyExists
@@ -23,6 +23,51 @@ class DriveFile:
     id: str
     path: str
     mime: str
+
+
+class DriveExport:
+    mime: str
+    ext: str
+
+    def __init__(self, mime: str, ext: str):
+        assert ext.startswith("."), "Extension must start with a dot"
+        self.mime = mime
+        self.ext = ext
+
+
+class DriveFileContext(GoogleImportingContext):
+    i: int
+    n: int
+    files: FileManager
+    session: Session
+    path: str
+    gfile: DriveFile
+
+    def __init__(self,
+        base: GoogleImportingContext,
+        i: int, n: int,
+        files: FileManager, session: Session,
+        path: str, gfile: DriveFile
+    ):
+        self._extend(base)
+        self.i = i
+        self.n = n
+        self.files = files
+        self.session = session
+        self.path = path
+        self.gfile = gfile
+
+    @property
+    def mime(self):
+        return self.gfile.mime
+    
+    @property
+    def google_id(self):
+        return self.gfile.id
+    
+    @property
+    def file_n(self):
+        return f"{self.i + 1}/{self.n}"
 
 
 class GoogleDriveImporter(GoogleImporter):
@@ -57,6 +102,56 @@ class GoogleDriveImporter(GoogleImporter):
         if gdrive_storages:
             return gdrive_storages[0]
         return storages.create("Google Drive")
+    
+    OD_SPREADSHEET_EXP = DriveExport("application/x-vnd.oasis.opendocument.spreadsheet", ".ods")
+    OD_TEXT_EXP = DriveExport("application/vnd.oasis.opendocument.text", ".odt")
+    OD_PRESENTATION_EXP = DriveExport("application/vnd.oasis.opendocument.presentation", ".odp")
+    PDF_EXP = DriveExport("application/pdf", ".pdf")
+    def __import_file(self, context: DriveFileContext):
+        match context.mime:
+            case "application/vnd.google-apps.folder":
+                logger.debug("Creating directory %s - %s", context.file_n, context.path)
+                return self.__import_dir(context)
+            case "application/vnd.google-apps.spreadsheet":
+                return self.__import_download(context, self.OD_SPREADSHEET_EXP)
+            case "application/vnd.google-apps.document":
+                return self.__import_download(context, self.OD_TEXT_EXP)
+            case "application/vnd.google-apps.presentation":
+                return self.__import_download(context, self.OD_PRESENTATION_EXP)
+            case "application/vnd.google-apps.drawing":
+                return self.__import_download(context, self.PDF_EXP)
+            case _:
+                return self.__import_download(context, None)
+
+    def __import_dir(self, context: DriveFileContext):
+        context.files.makedirs(fspath.dirname(context.path))
+        context.session.commit()
+
+    def __import_download(self, context: DriveFileContext, export: DriveExport|None):
+        try:
+            context.files.makedirs(fspath.dirname(context.path))
+            context.session.commit()
+            file = context.files.makefile(context.path, context.mime)
+            logger.debug("Downloading file %s - %s", context.file_n, context.path)
+            try:
+                if export is not None:
+                    request = context.service.files().export_media(fileId=context.google_id, mimeType=export.mime)  # type: ignore
+                    context.path += export.ext
+                else:
+                    request = context.service.files().get_media(fileId=context.google_id)  # type: ignore
+                buffer = io.BytesIO()
+                downloader = MediaIoBaseDownload(buffer, request)
+                done = False
+                while not done:
+                    status, done = downloader.next_chunk()
+                context.files.contents.write(file, buffer.getvalue())
+            except Exception as e:
+                logger.debug("Failed to download file %s - %s", context.file_n, str(e))
+                return
+            logger.debug("Finished downloading file %s", context.file_n)
+            context.session.commit()
+        except FileAlreadyExists:
+            return  # file already downloaded/exists
 
     def __download_files(self, gfiles: List[DriveFile], context: GoogleImportingContext):
         logger.debug("Downloading %d files", len(gfiles))
@@ -65,31 +160,8 @@ class GoogleDriveImporter(GoogleImporter):
             files = FileManager(context.files, context.user_id, session)
             for i, gfile in enumerate(gfiles):
                 path = fspath.join(storage.id, gfile.path)
-                if gfile.mime == "application/vnd.google-apps.folder":
-                    logger.debug("Creating directory %d/%d - %s", i + 1, len(gfiles), path)
-                    files.makedirs(fspath.dirname(path))
-                    session.commit()
-                else:
-                    try:
-                        files.makedirs(fspath.dirname(path))
-                        session.commit()
-                        file = files.makefile(path, gfile.mime)
-                        logger.debug("Downloading file %d/%d - %s", i + 1, len(gfiles), path)
-                        try:
-                            media = context.service.files().get_media(fileId=gfile.id)  # type: ignore
-                            buffer = io.BytesIO()
-                            downloader = MediaIoBaseDownload(buffer, media)
-                            done = False
-                            while not done:
-                                status, done = downloader.next_chunk()
-                            files.contents.write(file, buffer.getvalue())
-                        except Exception as e:
-                            logger.debug("Failed to download file %d/%d - %s", i + 1, len(gfiles), str(e))
-                            continue
-                        logger.debug("Finished downloading file %d/%d", i + 1, len(gfiles))
-                        session.commit()
-                    except FileAlreadyExists:
-                        continue  # file already downloaded/exists
+                gfile_context = DriveFileContext(context, i, len(gfiles), files, session, path, gfile)
+                self.__import_file(gfile_context)
 
     async def run(self, context: GoogleImportingContext):
         files: List[DriveFile] = []
