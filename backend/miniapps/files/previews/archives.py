1704ffcd695020f46f584b365277ceeb565cdc87
biser <bisserkr@gmail.com>
2024-01-14 14:37:20 +0200

    [Backend][Files] Archive file decompression API

--- backend/miniapps/files/previews/archives.py
+++ backend/miniapps/files/previews/archives.py
@@ -2,11 +2,8 @@ from dataclasses import dataclass
 import io
 from typing import List, BinaryIO
 
-import zipfile
-import gzip
-import tarfile
-import bz2
-import lzma
+import mimetypes
+mimetypes.guess_type
 
 
 @dataclass
@@ -17,58 +14,120 @@ class ArchiveFile:
 
 
 @dataclass
-class ArchivePreview:
+class ArchiveFileList:
     files: List[ArchiveFile]
 
 
-class ArchivePreviews:
-    @staticmethod
-    def __read_zip(blob: BinaryIO):
-        result: List[ArchiveFile] = []
-        with zipfile.ZipFile(blob) as zf:
-            for file in zf.filelist:
-                result.append(ArchiveFile(file.filename, size=file.file_size))
-        return ArchivePreview(result)
-    
-    @staticmethod
-    def __read_tar(blob: BinaryIO):
-        result: List[ArchiveFile] = []
-        with tarfile.open(fileobj=blob) as tf:
-            for file in tf.getmembers():
-                result.append(ArchiveFile(file.name, size=file.size))
-        return ArchivePreview(result)
-    
-    @staticmethod
-    def __read_gzip(blob: BinaryIO):
-        with gzip.open(blob) as fh:
-            bytes_io = io.BytesIO(fh.read())
-            return ArchivePreviews._archive_preview(bytes_io)
-    
-    @staticmethod
-    def __read_bz2(blob: BinaryIO):
-        with bz2.open(blob) as fh:
-            bytes_io = io.BytesIO(fh.read())
-            return ArchivePreviews._archive_preview(bytes_io)
-    
-    @staticmethod
-    def __read_lzma(blob: BinaryIO):
-        with lzma.open(blob) as fh:
-            bytes_io = io.BytesIO(fh.read())
-            return ArchivePreviews._archive_preview(bytes_io)
-    
-    PREVIEW_METHODS = [
-        __read_zip,
-        __read_gzip,
-        __read_tar,
-        __read_bz2,
-        __read_lzma,
-    ]
+@dataclass
+class ArchiveFileContent:
+    data: bytes
+    mime: str|None = None
 
+
+class UnknownArchiveError(Exception):
+    def __init__(self):
+        super().__init__("Unknown archive type")
+
+
+class ArchivePreview:
+    MULTIFILE: bool
+
+    blob: BinaryIO
+
+    def __init__(self, blob: BinaryIO):
+        self.blob = blob
+
+    def _unarchive(self) -> BinaryIO:
+        raise NotImplementedError()
+
+    def _list_files(self) -> List[ArchiveFile]:
+        raise NotImplementedError()
+    
+    def _read_file(self, path: str) -> ArchiveFileContent:
+        raise NotImplementedError()
+    
     @classmethod
-    def _archive_preview(cls, blob: BinaryIO):
-        for method in cls.PREVIEW_METHODS:
+    def _open(cls, blob: BinaryIO) -> "ArchivePreview":
+        for cls in ArchivePreview.__subclasses__():
             try:
-                return method(blob)
+                obj = cls(blob)
+                if obj.MULTIFILE:
+                   return obj
+                else:
+                    unarchived_blob = obj._unarchive()
+                    return cls._open(unarchived_blob)
             except:
                 pass
-        raise ValueError("Unsupported archive format")
+        raise UnknownArchiveError()
+    
+    @classmethod
+    def list_files(cls, blob: BinaryIO):
+        return ArchiveFileList(cls._open(blob)._list_files())
+    
+    @classmethod
+    def read_file(cls, blob: BinaryIO, path: str):
+        return cls._open(blob)._read_file(path)
+
+
+import zipfile
+class ZipArchivePreview(ArchivePreview):
+    MULTIFILE = True
+
+    def _list_files(self):
+        result: List[ArchiveFile] = []
+        with zipfile.ZipFile(self.blob) as zf:
+            for file in zf.filelist:
+                result.append(ArchiveFile(file.filename, size=file.file_size))
+        return result
+    
+    def _read_file(self, path: str):
+        with zipfile.ZipFile(self.blob) as zf:
+            with zf.open(path) as fh:
+                return ArchiveFileContent(fh.read())
+
+
+import tarfile
+class TarArchivePreview(ArchivePreview):
+    MULTIFILE = True
+
+    def _list_files(self):
+        result: List[ArchiveFile] = []
+        with tarfile.open(fileobj=self.blob) as tf:
+            for file in tf.getmembers():
+                result.append(ArchiveFile(file.name, size=file.size))
+        return result
+    
+    def _read_file(self, path: str):
+        with tarfile.open(fileobj=self.blob) as tf:
+            fh = tf.extractfile(path)
+            if fh is None:
+                raise FileNotFoundError(path)
+            with fh:
+                return ArchiveFileContent(fh.read())
+
+
+import gzip
+class GZipArchivePreview(ArchivePreview):
+    MULTIFILE = False
+
+    def _unarchive(self):
+        with gzip.open(self.blob) as fh:
+            return io.BytesIO(fh.read())
+
+
+import bz2
+class Bz2ArchivePreview(ArchivePreview):
+    MULTIFILE = False
+    
+    def _unarchive(self):
+        with bz2.open(self.blob) as fh:
+            return io.BytesIO(fh.read())
+
+
+import lzma
+class LzmaArchivePreview(ArchivePreview):
+    MULTIFILE = False
+    
+    def _unarchive(self):
+        with lzma.open(self.blob) as fh:
+            return io.BytesIO(fh.read())
--- backend/miniapps/files/previews/module.py
+++ backend/miniapps/files/previews/module.py
@@ -1,13 +1,14 @@
-from .archives import ArchivePreviews, ArchivePreview
+from .archives import ArchiveFileList, ArchivePreview
 
 from ..tools.files import FileManager
 
+from core.api.modules.rest import ApiResponse, RestMiniappModule, get
 from core.api.modules.gql import GqlMiniappModule, query
 from core.auth.handlers import AuthError
 from core.data.blobs.base import OpenMode
 
 
-class PreviewModule(GqlMiniappModule, ArchivePreviews):
+class PreviewModule(GqlMiniappModule):
     _manager: FileManager|None = None
 
     @property
@@ -19,9 +20,33 @@ class PreviewModule(GqlMiniappModule, ArchivePreviews):
         return self._manager
 
     @query()
-    def archive(self, path: str) -> ArchivePreview:
+    def archive(self, path: str) -> ArchiveFileList:
         file = self.manager.by_path(path)
         if file is None:
             raise FileNotFoundError(path)
         with self.manager.contents.open(file, OpenMode.READ) as blob:
-            return self._archive_preview(blob)
+            return ArchivePreview.list_files(blob)
+
+
+class PreviewContentsModule(RestMiniappModule):
+    _manager: FileManager|None = None
+
+    @property
+    def manager(self):
+        if self._manager is None:
+            if self.user_id is None:
+                raise AuthError()
+            self._manager = FileManager(self.context.blobs, self.user_id, self.session)
+        return self._manager
+
+    @get("/api/files/preview/(.*)", name="files.preview.read")
+    def archive_file(self, path: str):
+        file = self.manager.by_path(path)
+        if file is None:
+            raise FileNotFoundError(path)
+        with self.manager.contents.open(file, OpenMode.READ) as blob:
+            file_contents = ArchivePreview.read_file(blob, path)
+            response = ApiResponse()
+            response.content = file_contents.data
+            response.add_header("Content-Type", file_contents.mime or "application/octet-stream")
+            return response
