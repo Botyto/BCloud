c9f9e164bf4820f1ed0a0e8639e5ac4f4667c10b
biser <bisserkr@gmail.com>
2024-01-26 20:31:44 +0200

    [Backend][Notes] Rewrite google importing

--- backend/miniapps/notes/importing.py
+++ backend/miniapps/notes/importing.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
 import logging
-import pickle
 from typing import Any, List
 
 from core.data.sql.database import Session
 
-from core.data.blobs.base import OpenMode
 from miniapps.profile.importing.google import GoogleImporter, GoogleImportingContext
+from miniapps.profile.importing.google_items import GoogleItem, GoogleItemContext, GoogleItemImporter
 
 from .data import NotesCollection, NotesNote, NotesCollectionView, NotesFileKind
 from .tools.collections import CollectionsManager
@@ -29,7 +28,7 @@ class GNoteListItem:
 
 
 @dataclass
-class GNote:
+class GNote(GoogleItem):
     name: str
     title: str
     content: str
@@ -37,51 +36,24 @@ class GNote:
     trashed: bool
     attachments: List[GNoteAttachment]
 
-
-class KeepNoteContext(GoogleImportingContext):
-    i: int
-    n: int
-    notes: NotesManager
-    session: Session
-    collection: NotesCollection
-    gnote: GNote
-
-    def __init__(self,
-        base: GoogleImportingContext,
-        i: int, n: int,
-        notes: NotesManager,
-        session: Session,
-        collection: NotesCollection,
-        gnote: GNote,
-    ):
-        self._extend(base)
-        self.i = i
-        self.n = n
-        self.notes = notes
-        self.session = session
-        self.collection = collection
-        self.gnote = gnote
-
     @property
     def google_name(self):
-        return self.gnote.name
+        return self.name
 
-    @property
-    def note_n(self):
-        return f"{self.i + 1}/{self.n}"
+
+@dataclass
+class NoteHelper:
+    manager: NotesManager
+    collection: NotesCollection
 
 
 # As of writing this, the Google Keep API is intended only for enterprise users.
 # This question mentions a link to a ticket addressing this:
 # https://stackoverflow.com/questions/70312083/google-keep-api-responds-with-invalid-scope-when-using-documented-scopes
-class GoogleKeepImporter(GoogleImporter):
-    NAME = "keep"
-    SERVICE = "keep"
-    VERSION = "v1"
-    SCOPES = {
-        "https://www.googleapis.com/auth/keep.readonly",
-    }
-
+class KeepItemImporter(GoogleItemImporter):
+    def gather_page_next(self, page_token: str|None):
+        return self.context.service.notes().list(filter="", pageSize=100, pageToken=page_token).execute()
+    
     def __parse_list_item(self, item: Any) -> GNoteListItem:
         children = []
         if "childListItems" in item:
@@ -92,51 +64,51 @@ class GoogleKeepImporter(GoogleImporter):
             children=children,
         )
 
-    def __gather_notes(self, output: list, context: GoogleImportingContext):
-        next_page_token: str|None = None
-        while True:
-            results = context.service.notes().list(filter="", pageSize=100, pageToken=next_page_token).execute()
-            next_page_token = results.get("nextPageToken", None)
-            items = results.get("notes", [])
-            for item in items:
-                attachments = [
-                    GNoteAttachment(
-                        name=att["name"],
-                        mime_type=att["mimeType"],
-                    )
-                    for att in item.get("attachments", [])
-                ]
-                content: str = ""
-                checklist: List[GNoteListItem] = []
-                if "text" in item["body"]:
-                    content = item["body"]["text"]["text"]
-                    checklist = []
-                elif "list" in item["body"]:
-                    content = ""
-                    checklist = [
-                        self.__parse_list_item(list_item)
-                        for list_item in item["body"]["list"]["listItems"]
-                    ]
-                note = GNote(
-                    name=item["name"],
-                    title=item["title"],
-                    content=content,
-                    checklist=checklist,
-                    trashed=item["trashed"],
-                    attachments=attachments,
+    def gather_page_process(self, output: List[GNote], response: dict):
+        items = response.get("notes", [])
+        for item in items:
+            attachments = [
+                GNoteAttachment(
+                    name=att["name"],
+                    mime_type=att["mimeType"],
                 )
-                output.append(note)
-            if next_page_token is None or not items:
-                break
+                for att in item.get("attachments", [])
+            ]
+            content: str = ""
+            checklist: List[GNoteListItem] = []
+            if "text" in item["body"]:
+                content = item["body"]["text"]["text"]
+                checklist = []
+            elif "list" in item["body"]:
+                content = ""
+                checklist = [
+                    self.__parse_list_item(list_item)
+                    for list_item in item["body"]["list"]["listItems"]
+                ]
+            note = GNote(
+                name=item["name"],
+                title=item["title"],
+                content=content,
+                checklist=checklist,
+                trashed=item["trashed"],
+                attachments=attachments,
+            )
+            output.append(note)
 
     COLLECTION_NAME = "Google Keep"
-    def __get_gkeep_collection(self, context: GoogleImportingContext, session: Session):
-        collections = CollectionsManager(context.user_id, session)
+    def __get_gkeep_collection(self, session: Session):
+        collections = CollectionsManager(self.context.user_id, session)
         gkeep_collections = collections.by_name(self.COLLECTION_NAME, True)
         if gkeep_collections:
             return gkeep_collections[0]  # type: ignore
         return collections.create(self.COLLECTION_NAME, None, NotesCollectionView.NOTES)
 
+    def create_helper(self, session: Session):
+        return NoteHelper(
+            manager=NotesManager(self.context.user_id, self.context, session),
+            collection=self.__get_gkeep_collection(session),
+        )
+    
     def __checklist_to_md(self, checklist: List[GNoteListItem], indent: int = 0) -> str:
         output = []
         for item in checklist:
@@ -146,51 +118,39 @@ class GoogleKeepImporter(GoogleImporter):
             output.extend(self.__checklist_to_md(item.children, indent + 1))
         return "\n".join(output)
 
-    def __download_attachment(self, context: KeepNoteContext, gattachment: GNoteAttachment, note: NotesNote):
-        result = context.service.media().download_media(name=gattachment.name, mimeType=gattachment.mime_type).execute()
-        context.notes.files.default_write(note.id, result, gattachment.mime_type, NotesFileKind.ATTACHMENT)
+    def __download_attachment(self, gnote_context: GoogleItemContext[GNote, NoteHelper], gattachment: GNoteAttachment, note: NotesNote):
+        content = gnote_context.service.media() \
+            .download_media(name=gattachment.name, mimeType=gattachment.mime_type) \
+            .execute()
+        gnote_context.helper.manager.files.default_write(note.id, content, gattachment.mime_type, NotesFileKind.ATTACHMENT)
 
-    def __import_note(self, context: KeepNoteContext):
-        logger.debug("Importing note %s - %s", context.note_n, context.google_name)
-        if context.gnote.checklist:
-            content = self.__checklist_to_md(context.gnote.checklist)
+    def import_item(self, gnote_context: GoogleItemContext[GNote, NoteHelper]):
+        if gnote_context.item.checklist:
+            content = self.__checklist_to_md(gnote_context.item.checklist)
         else:
-            content = context.gnote.content
-        note = context.notes.create(
-            collection_id_or_slug=context.collection.id,
-            title=context.gnote.title,
+            content = gnote_context.item.content
+        note = gnote_context.helper.manager.create(
+            collection_id_or_slug=gnote_context.helper.collection.id,
+            title=gnote_context.item.title,
             content=content,
             tags=[],
         )
-        note.archived = context.gnote.trashed
-        for i, gattachment in enumerate(context.gnote.attachments):
-            self.__download_attachment(context, gattachment, note)
+        note.archived = gnote_context.item.trashed
+        for i, gattachment in enumerate(gnote_context.item.attachments):
+            self.__download_attachment(gnote_context, gattachment, note)
             if i > 1:
-                logger.warning("Note %s - %s has more than 1 attachment", context.note_n, context.google_name)
+                logger.warning("Note %s - %s has more than 1 attachment", gnote_context.item_num, gnote_context.item.google_name)
                 break
 
-    def __create_notes(self, gnotes: List[GNote], context: GoogleImportingContext):
-        logger.debug("Importing %d notes", len(gnotes))
-        with context.database.make_session() as session:
-            collection = self.__get_gkeep_collection(context, session)
-            notes = NotesManager(context.user_id, context, session)
-            for i, gnote in enumerate(gnotes):
-                gnote_context = KeepNoteContext(context, i, len(gnotes), notes, session, collection, gnote)
-                self.__import_note(gnote_context)
+
+class GoogleKeepImporter(GoogleImporter):
+    NAME = "keep"
+    SERVICE = "keep"
+    VERSION = "v1"
+    SCOPES = {
+        "https://www.googleapis.com/auth/keep.readonly",
+    }
 
     async def run(self, context: GoogleImportingContext):
-        notes: List[GNote] = []
-        cache_addr = context.temp_file_addr("gkeep_import", "notes.pickle")
-        if context.blobs.exists(cache_addr):
-            logger.debug("Loading cached notes")
-            with context.blobs.open(cache_addr, OpenMode.READ) as fh:
-                notes = pickle.load(fh)
-        else:
-            logger.debug("Gethering notes")
-            self.__gather_notes(notes, context)
-            notes.sort(key=lambda f: f.name)
-            with context.blobs.open(cache_addr, OpenMode.WRITE) as fh:
-                pickle.dump(notes, fh)
-        self.__create_notes(notes, context)
-        logger.debug("Finished importing")
-        # TODO delete cache file
+        keep = KeepItemImporter(logger, context)
+        await keep.run()
